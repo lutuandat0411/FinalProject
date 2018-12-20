2e5c7c6e92482a8a6a59ec008652b2caa9b58b87
nguyenkhang98 <nguyenthaikhang0509@gmail.com>
2018-12-20 17:01:35 +0700

    Unit test Index

---
+++ DIENMAYQUYETTIENTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using DIENMAYQUYETTIEN.Controllers;
+
+namespace DIENMAYQUYETTIENTest
+{
+    [TestClass]
+    public class TestHomeController
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
