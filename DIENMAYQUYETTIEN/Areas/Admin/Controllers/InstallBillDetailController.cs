e9e61a0cad61ebdf9fd9e0aa48f365b2173bb4b4
Loc Dep Trai <phanphuocloc135@gmail.com>
2018-12-27 19:03:28 +0700

    installment bill update

---
+++ DIENMAYQUYETTIEN/Areas/Admin/Controllers/InstallBillDetailController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DIENMAYQUYETTIEN.Models;
+
+namespace DIENMAYQUYETTIEN.Areas.Admin.Controllers
+{
+    public class InstallBillDetailController : Controller
+    {
+        private DIENMAYQUYETTIENEntities db = new DIENMAYQUYETTIENEntities();
+
+        // GET: /Admin/InstallBillDetail/
+        public ActionResult Index()
+        {
+            var installmentbilldetails = db.InstallmentBillDetails.Include(i => i.InstallmentBill).Include(i => i.Product);
+            return View(installmentbilldetails.ToList());
+        }
+
+        // GET: /Admin/InstallBillDetail/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBillDetail installmentbilldetail = db.InstallmentBillDetails.Find(id);
+            if (installmentbilldetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installmentbilldetail);
+        }
+
+        // GET: /Admin/InstallBillDetail/Create
+        public ActionResult Create()
+        {
+            ViewBag.BillID = new SelectList(db.InstallmentBills, "ID", "BillCode");
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "ProductCode");
+            return View();
+        }
+
+        // POST: /Admin/InstallBillDetail/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,BillID,ProductID,Quantity,InstallmentPrice")] InstallmentBillDetail installmentbilldetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.InstallmentBillDetails.Add(installmentbilldetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BillID = new SelectList(db.InstallmentBills, "ID", "BillCode", installmentbilldetail.BillID);
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "ProductCode", installmentbilldetail.ProductID);
+            return View(installmentbilldetail);
+        }
+
+        // GET: /Admin/InstallBillDetail/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBillDetail installmentbilldetail = db.InstallmentBillDetails.Find(id);
+            if (installmentbilldetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BillID = new SelectList(db.InstallmentBills, "ID", "BillCode", installmentbilldetail.BillID);
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "ProductCode", installmentbilldetail.ProductID);
+            return View(installmentbilldetail);
+        }
+
+        // POST: /Admin/InstallBillDetail/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,BillID,ProductID,Quantity,InstallmentPrice")] InstallmentBillDetail installmentbilldetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(installmentbilldetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BillID = new SelectList(db.InstallmentBills, "ID", "BillCode", installmentbilldetail.BillID);
+            ViewBag.ProductID = new SelectList(db.Products, "ID", "ProductCode", installmentbilldetail.ProductID);
+            return View(installmentbilldetail);
+        }
+
+        // GET: /Admin/InstallBillDetail/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBillDetail installmentbilldetail = db.InstallmentBillDetails.Find(id);
+            if (installmentbilldetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installmentbilldetail);
+        }
+
+        // POST: /Admin/InstallBillDetail/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            InstallmentBillDetail installmentbilldetail = db.InstallmentBillDetails.Find(id);
+            db.InstallmentBillDetails.Remove(installmentbilldetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DIENMAYQUYETTIEN/Areas/Admin/Controllers/InstallmentBillController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DIENMAYQUYETTIEN.Models;
+
+namespace DIENMAYQUYETTIEN.Areas.Admin.Controllers
+{
+    public class InstallmentBillController : Controller
+    {
+        private DIENMAYQUYETTIENEntities db = new DIENMAYQUYETTIENEntities();
+
+        // GET: /Admin/InstallmentBill/
+        public ActionResult Index()
+        {
+            var installmentbills = db.InstallmentBills.Include(i => i.Customer);
+            return View(installmentbills.ToList());
+        }
+
+        // GET: /Admin/InstallmentBill/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBill installmentbill = db.InstallmentBills.Find(id);
+            if (installmentbill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installmentbill);
+        }
+
+        // GET: /Admin/InstallmentBill/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode");
+            return View();
+        }
+
+        // POST: /Admin/InstallmentBill/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,BillCode,CustomerID,Date,Shipper,Note,Method,Period,GrandTotal,Taken,Remain")] InstallmentBill installmentbill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.InstallmentBills.Add(installmentbill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode", installmentbill.CustomerID);
+            return View(installmentbill);
+        }
+
+        // GET: /Admin/InstallmentBill/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBill installmentbill = db.InstallmentBills.Find(id);
+            if (installmentbill == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode", installmentbill.CustomerID);
+            return View(installmentbill);
+        }
+
+        // POST: /Admin/InstallmentBill/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,BillCode,CustomerID,Date,Shipper,Note,Method,Period,GrandTotal,Taken,Remain")] InstallmentBill installmentbill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(installmentbill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "ID", "CustomerCode", installmentbill.CustomerID);
+            return View(installmentbill);
+        }
+
+        // GET: /Admin/InstallmentBill/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            InstallmentBill installmentbill = db.InstallmentBills.Find(id);
+            if (installmentbill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(installmentbill);
+        }
+
+        // POST: /Admin/InstallmentBill/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            InstallmentBill installmentbill = db.InstallmentBills.Find(id);
+            db.InstallmentBills.Remove(installmentbill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
