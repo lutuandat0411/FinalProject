a8807d304547949088bbe546bf68065ddf213e3c
Van Trung <vantrung02101998@gmail.com>
2018-12-29 00:27:55 +0700

    unit test create product

--- DIENMAYQUYETTIENTest/BangsanphamTest.cs
+++ DIENMAYQUYETTIENTest/BangsanphamTest.cs
@@ -43,21 +43,54 @@ namespace DIENMAYQUYETTIENTest
         }
 
         [TestMethod]
-        public void CreateTest()
+        public void CreateGetTest()
+        {
+            var controller = new ProductAdminController();
+            var result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.ViewData["ProductType"], typeof(SelectList));
+        }
+        [TestMethod]
+        public void CreatePostTest()
         {
             var controller = new ProductAdminController();
             var context = new Mock<HttpContextBase>();
-            context.Setup(c => c.Session["UserName"]).Returns("abc");
+            var request = new Mock<HttpRequestBase>();
+            var files = new Mock<HttpFileCollectionBase>();
+            var file = new Mock<HttpPostedFileBase>();
+            context.Setup(c => c.Request).Returns(request.Object);
+            request.Setup(r => r.Files).Returns(files.Object);
+            files.Setup(f => f["Imagefile"]).Returns(file.Object);
+            file.Setup(f => f.ContentLength).Returns(1);
+            context.Setup(c => c.Server.MapPath("~/Image")).Returns("~/Image");
             controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
 
-            var result = controller.Index() as ViewResult;
             var db = new DIENMAYQUYETTIENEntities();
 
-            Assert.IsNotNull(result);
-           
+
+            using (var scope = new TransactionScope())
+            {
+                var model = new Product();
+                model.ProductTypeID = db.ProductTypes.First().ID;
+                model.ProductName = "TenSP";
+                model.OriginPrice = 123;
+                model.SalePrice = 456;
+                model.InstallmentPrice = 789;
+                model.Quantity = 10;
+                var result0 = controller.Create(model) as RedirectToRouteResult;
+                //Assert.IsNotNull(result0);
+                file.Verify(f => f.SaveAs(It.Is<string>(s => s.StartsWith("~/Image/"))));
+                Assert.AreEqual("Index", result0.RouteValues["action"]);
+
+                file.Setup(f => f.ContentLength).Returns(0);
+                var result1 = controller.Create(model) as ViewResult;
+                Assert.IsNotNull(result1);
+                Assert.IsInstanceOfType(result1.ViewData["ProductType"], typeof(SelectList));
+            }
         }
 
-       
+
         [TestMethod]
            public void DeleteTest()
            {
