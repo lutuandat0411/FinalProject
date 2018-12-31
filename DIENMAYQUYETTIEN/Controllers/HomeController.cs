5682511d3b41d6ec1cdedf200027d2cd9dd344ff
Lu Tuan Dat <drianliz0411@gmail.com>
2018-12-31 21:32:38 +0700

    Chưa hoàn thành

--- DIENMAYQUYETTIEN/Controllers/HomeController.cs
+++ DIENMAYQUYETTIEN/Controllers/HomeController.cs
@@ -2,27 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DIENMAYQUYETTIEN.Models;
 using System.Web.Mvc;
 
 namespace DIENMAYQUYETTIEN.Controllers
 {
     public class HomeController : Controller
     {
+        DIENMAYQUYETTIENEntities db = new DIENMAYQUYETTIENEntities();
         public ActionResult Index()
         {
-            return View();
+            var product = db.Products.OrderByDescending(x => x.ID).ToList();
+            return View(product);
         }
-
+        public FileResult LoadImage(String id)
+        {
+            var path = Server.MapPath("~/Image/" + id);
+            return File(path, "Imagefile");
+        }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Install()
+        {
+
+            return View();
+        }
+        public ActionResult News()
+        {
 
             return View();
         }
